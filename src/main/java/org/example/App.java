c80c2a3a667d4ed5c0137e789b555a2fcceb516a
EV-02 <ayeekee@knights.ucf.edu>
2021-09-11 22:02:02 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 14 Solution
+ *  Copyright 2021 Alyssa Yee-Kee
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner se = new Scanner(System.in);
+
+        String key = "WI";
+
+        System.out.print("What is the order amount? ");
+        String orderAmt = se.nextLine();
+        double order = Double.parseDouble(orderAmt);
+
+        System.out.print("What is the state? ");
+        String state = se.nextLine();
+
+        double tax = 0;
+        double total = order;
+
+        if (state.equalsIgnoreCase(key)){
+            tax = order * 0.055;
+            total += tax;
+
+            System.out.println("The subtotal is $" + String.format("%.2f", order) + ".\nThe tax is $" + tax + ".");
+        }
+
+        System.out.println("The total is $" + String.format("%.2f", total));
+    }
+}
